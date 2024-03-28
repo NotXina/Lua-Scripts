d05bf411e7a7a43dbb165c799501c106686f5d17
NotXina <157437731+NotXina@users.noreply.github.com>
2024-03-28 12:00:41 -0300

    Update XinaPackNew.lua

--- XinaPackNew.lua
+++ XinaPackNew.lua
@@ -35,10 +35,6 @@ Pally_PvP.setOn(isOn)
 end)
 
 
-
-
-
-
 local wand = 651 -- wand ID 
 local itemsIds = {3554, 3369, 3567, 3420, 3414, 3366, 3364, 3392, 3320, 3281, 7407, 7387, 3079, 826, 10387, 10385, 3006, 3360, 3324, 3381, 3382, 3419, 3385} -- ID items to sell 
 Seller = addIcon("Seller", {item = 651, text = "Seller"},
@@ -86,184 +82,6 @@ macro(50,function()
   end
 end)
 
-
-
-
--- Fim dos ICONES
-
-UI.Separator()
-
-local name = ""..g_game.getCharacterName()..""
-local panelName = "targetLeader"
-local toAttack = nil
-
-if not storage[panelName .. name] then
-storage[panelName .. name] = {
-    attackLeader = "nome do Lider"
-}
-end
-
-onMissle(function(missle)
-    if not storage[panelName .. name].attackLeader or storage[panelName .. name].attackLeader:len() == 0 then
-        return
-    end
-    local src = missle:getSource()
-    if src.z ~= posz() then
-        return
-    end
-    local from = g_map.getTile(src)
-    local to = g_map.getTile(missle:getDestination())
-    if not from or not to then
-        return
-    end
-    local fromCreatures = from:getCreatures()
-    local toCreatures = to:getCreatures()
-    if #fromCreatures ~= 1 or #toCreatures ~= 1 then
-        return
-    end
-    local c1 = fromCreatures[1]
-    if c1:getName():lower() == storage[panelName .. name].attackLeader:lower() then
-        toAttack = toCreatures[1]
-    end
-end)
-
-macro(50, "Attack Leader", nil, function()
-    if isInPz() then return end
-    if toAttack and storage[panelName .. name].attackLeader:len() > 0 and toAttack ~= g_game.getAttackingCreature() then
-        if toAttack:getName() ~= storage[panelName .. name].attackLeader then
-            g_game.cancelAttack()
-            g_game.attack(toAttack)
-            toAttack = nil
-        end
-    end
-end)
-
-	
-
-addTextEdit("attackLeader", storage[panelName .. name].attackLeader or "Nome do Lider", function(widget, lidertext)
-    storage[panelName .. name].attackLeader = lidertext
-end)
-
-UI.Separator()
-
-
-UI.Label("Ataca os Players em sua Enemy List")
-
-atack_enemy = macro(100, "Attack Players","Delete", function()
- local highestAmount = 100  
-  local targetPlayer
-  if isInPz() then return end
-  for i, creature in ipairs(getSpectators(posz(), false)) do
-    if creature:isPlayer() then
-      local cname = creature:getName()
-      if isEnemy(cname) then    
-        --if creature:getShield() < 3 and creature:getEmblem() ~= 1 then
-          local valHp = creature:getHealthPercent()
-          if valHp <= highestAmount then
-            local cPos = creature:getPosition()
-            local cTile = g_map.getTile(cPos)
-            if cTile and cTile:canShoot() then
-              highestAmount = valHp
-              targetPlayer = creature
-            end
-          end
-        end
-      end
-    end
- -- end
-  if targetPlayer then
-    if not g_game.isAttacking() or g_game.getAttackingCreature() ~= targetPlayer then
-      g_game.attack(targetPlayer)
-    end
-  end
-end)
-
-addIcon("Atk_enemy", {item=5668, text="Kill"}, function(icon, isOn)
-atack_enemy.setOn(isOn)
-end)
-
-
-
-UI.Separator()
-
-UI.Label("Use MW")
-
-local toggle = macro(10, "Mwall Step", "F12",function() end) -- taca mw no sqm anterior
-
-onPlayerPositionChange(function(newPos, oldPos)
-    if oldPos.z ~= posz() then return end
-    if oldPos then
-        local tile = g_map.getTile(oldPos)
-        if toggle.isOn() and tile:isWalkable() then
-            useWith(3180, tile:getTopUseThing())
-            toggle.setOff()
-        end
-    end
-end)
-
-local toggle2 = macro(10, "MW Target Step", "F11", function() end) -- taca mw no sqm onde o alvo esta
-
-onCreaturePositionChange(function(creature, newPos, oldPos)
-    if creature == target() or creature == g_game.getFollowingCreature() then
-        if oldPos and oldPos.z == posz() then
-            local tile2 = g_map.getTile(oldPos)
-            if toggle2.isOn() and tile2:isWalkable() then
-                useWith(3180, tile2:getTopUseThing())
-                toggle2.setOff()
-            end 
-        end
-    end
-end)
-
-
-UI.Separator()
-
-local dropItems = { 3492, 3031 }
-local maxStackedItems = 10
-local dropDelay = 600
-
-gpAntiPushDrop = macro(dropDelay , "anti push", function ()
-  antiPush()
-end)
-
-onPlayerPositionChange(function()
-    antiPush()
-end)
-
-function antiPush()
-  if gpAntiPushDrop:isOff() then
-    return
-  end
-
-  local tile = g_map.getTile(pos())
-  if tile and tile:getThingCount() < maxStackedItems then
-    local thing = tile:getTopThing()
-    if thing and not thing:isNotMoveable() then
-      for i, item in pairs(dropItems) do
-        if item ~= thing:getId() then
-            local dropItem = findItem(item)
-            if dropItem then
-              g_game.move(dropItem, pos(), 1)
-            end
-        end
-      end
-    end
-  end
-end
-
-local function checkPos(x, y)
- xyz = g_game.getLocalPlayer():getPosition()
- xyz.x = xyz.x + x
- xyz.y = xyz.y + y
- tile = g_map.getTile(xyz)
- if tile then
-  return g_game.use(tile:getTopUseThing())  
- else
-  return false
- end
-end
-
-
 bugMap = macro(1, function() 
  if modules.corelib.g_keyboard.isKeyPressed('Up') or modules.corelib.g_keyboard.isKeyPressed('w')  then
   checkPos(0, -5)
@@ -281,112 +99,6 @@ bugMapIcon = addIcon("Bug Map", {item=3368, text="DASH", hotkey="NumPad0"}, func
   modules.game_console.consoleTextEdit:setVisible(not isOn)
   bugMap.setOn(isOn)
  end)
-
-
-function staminaItems(parent)
-  if not parent then
-    parent = panel
-  end
-  local panelName = "staminaItemsUser"
-  local ui = setupUI([[
-Panel
-  height: 65
-  margin-top: 2
-
-  SmallBotSwitch
-    id: title
-    anchors.left: parent.left
-    anchors.right: parent.right
-    anchors.top: parent.top
-    text-align: center
-
-  HorizontalScrollBar
-    id: scroll1
-    anchors.left: parent.left
-    anchors.right: parent.horizontalCenter
-    anchors.top: title.bottom
-    margin-right: 2
-    margin-top: 2
-    minimum: 0
-    maximum: 42
-    step: 1
-    
-  HorizontalScrollBar
-    id: scroll2
-    anchors.left: parent.horizontalCenter
-    anchors.right: parent.right
-    anchors.top: prev.top
-    margin-left: 2
-    minimum: 0
-    maximum: 42
-    step: 1    
-
-  ItemsRow
-    id: items
-    anchors.left: parent.left
-    anchors.right: parent.right
-    anchors.top: prev.bottom
-  ]], parent)
-  ui:setId(panelName)
-
-  if not storage[panelName] then
-    storage[panelName] = {
-      min = 0,
-      max = 40,
-    }
-  end
-
-  local updateText = function()
-    ui.title:setText("" .. storage[panelName].min .. " <= Stamina >= " .. storage[panelName].max .. "")  
-  end
- 
-  ui.scroll1.onValueChange = function(scroll, value)
-    storage[panelName].min = value
-    updateText()
-  end
-  ui.scroll2.onValueChange = function(scroll, value)
-    storage[panelName].max = value
-    updateText()
-  end
- 
-  ui.scroll1:setValue(storage[panelName].min)
-  ui.scroll2:setValue(storage[panelName].max)
- 
-  ui.title:setOn(storage[panelName].enabled)
-  ui.title.onClick = function(widget)
-    storage[panelName].enabled = not storage[panelName].enabled
-    widget:setOn(storage[panelName].enabled)
-  end
- 
-  if type(storage[panelName].items) ~= 'table' then
-    storage[panelName].items = { 11588 }
-  end
- 
-  for i=1,5 do
-    ui.items:getChildByIndex(i).onItemChange = function(widget)
-      storage[panelName].items[i] = widget:getItemId()
-    end
-    ui.items:getChildByIndex(i):setItemId(storage[panelName].items[i])    
-  end
- 
-  macro(500, function()
-    if not storage[panelName].enabled or stamina() / 60 < storage[panelName].min or stamina() / 60 > storage[panelName].max then
-      return
-    end
-    local candidates = {}
-    for i, item in pairs(storage[panelName].items) do
-      if item >= 100 then
-        table.insert(candidates, item)
-      end
-    end
-    if #candidates == 0 then
-      return
-    end    
-    use(candidates[math.random(1, #candidates)])
-  end)
-end
-staminaItems()
-
 
 local cIcon = addIcon("cI",{text="Cave\nBot",switchable=false,moveable=true}, function()
   if CaveBot.isOff() then 
@@ -423,8 +135,10 @@ end)
 
 
 
+-- Fim dos ICONES
 
-  local panelName = "advancedFriendHealer"
+
+ local panelName = "advancedFriendHealer"
   local ui = setupUI([[
 Panel
   height: 19
@@ -679,36 +393,123 @@ addSeparator()
 
 
 
- 
- 
- 
- 
- 
- 
- 
- 
-local UE = "Magia de Area"
+UI.Separator()
 
-if not storage[UE .. name] then
-storage[UE .. name] = {
-    AOEspell = "Exevo gran mas pox"
+local name = ""..g_game.getCharacterName()..""
+local panelName = "targetLeader"
+local toAttack = nil
+
+if not storage[panelName .. name] then
+storage[panelName .. name] = {
+    attackLeader = "nome do Lider"
 }
 end
 
-macro(1000, "Safe SD/Mas Vis", function()
-  local target = g_game.getAttackingCreature()
-  if not target then return end
-  if isSafe(8) and manapercent() >= 70 then
-    say(storage[UE .. name].AOEspell)
-  else
-    usewith(3155, g_game.getAttackingCreature())
+onMissle(function(missle)
+    if not storage[panelName .. name].attackLeader or storage[panelName .. name].attackLeader:len() == 0 then
+        return
+    end
+    local src = missle:getSource()
+    if src.z ~= posz() then
+        return
+    end
+    local from = g_map.getTile(src)
+    local to = g_map.getTile(missle:getDestination())
+    if not from or not to then
+        return
+    end
+    local fromCreatures = from:getCreatures()
+    local toCreatures = to:getCreatures()
+    if #fromCreatures ~= 1 or #toCreatures ~= 1 then
+        return
+    end
+    local c1 = fromCreatures[1]
+    if c1:getName():lower() == storage[panelName .. name].attackLeader:lower() then
+        toAttack = toCreatures[1]
+    end
+end)
+
+macro(50, "Attack Leader", nil, function()
+    if isInPz() then return end
+    if toAttack and storage[panelName .. name].attackLeader:len() > 0 and toAttack ~= g_game.getAttackingCreature() then
+        if toAttack:getName() ~= storage[panelName .. name].attackLeader then
+            g_game.cancelAttack()
+            g_game.attack(toAttack)
+            toAttack = nil
+        end
+    end
+end)
+
+local specName = "alvo"
+
+if not storage[specName .. name] then
+storage[specName .. name] = {
+    specplayer = "Alvo"
+}
+end
+
+macro(100, "Atack Fixed Player", function()
+
+if not g_game.isAttacking() or g_game.getAttackingCreature():getName() ~= storage[specName .. name].specplayer then
+
+
+       for i, spec in ipairs(getSpectators()) do
+		local xname = spec:getName()
+			 if spec:isPlayer() and spec:getName() == storage[specName .. name].specplayer then
+                g_game.attack(spec)
+            end
+		end
+		
+end		
+		
+end) 
+
+addTextEdit("specplayer", storage[specName .. name].specplayer or "alvo", function(widget, alvotext)
+    storage[specName .. name].specplayer = alvotext
+end)
+	
+
+addTextEdit("attackLeader", storage[panelName .. name].attackLeader or "Nome do Lider", function(widget, lidertext)
+    storage[panelName .. name].attackLeader = lidertext
+end)
+
+UI.Separator()
+
+
+UI.Label("Ataca os Players em sua Enemy List")
+
+atack_enemy = macro(100, "Attack Players","Delete", function()
+ local highestAmount = 100  
+  local targetPlayer
+  if isInPz() then return end
+  for i, creature in ipairs(getSpectators(posz(), false)) do
+    if creature:isPlayer() then
+      local cname = creature:getName()
+      if isEnemy(cname) then    
+        --if creature:getShield() < 3 and creature:getEmblem() ~= 1 then
+          local valHp = creature:getHealthPercent()
+          if valHp <= highestAmount then
+            local cPos = creature:getPosition()
+            local cTile = g_map.getTile(cPos)
+            if cTile and cTile:canShoot() then
+              highestAmount = valHp
+              targetPlayer = creature
+            end
+          end
+        end
+      end
+    end
+ -- end
+  if targetPlayer then
+    if not g_game.isAttacking() or g_game.getAttackingCreature() ~= targetPlayer then
+      g_game.attack(targetPlayer)
+    end
   end
 end)
 
-addTextEdit("AOEspell", storage[UE .. name].AOEspell or "Exevo gran mas pox", function(widget, uetext)
-    storage[UE .. name].AOEspell = uetext
+addIcon("Atk_enemy", {item=5668, text="Kill"}, function(icon, isOn)
+atack_enemy.setOn(isOn)
 end)
-
 
 
 local specName = "alvo"
@@ -737,6 +538,225 @@ end)
 
 addTextEdit("specplayer", storage[specName .. name].specplayer or "alvo", function(widget, alvotext)
     storage[specName .. name].specplayer = alvotext
+end)
+
+
+
+
+
+
+UI.Separator()
+
+UI.Label("Use MW")
+
+local toggle = macro(10, "Mwall Step", "F12",function() end) -- taca mw no sqm anterior
+
+onPlayerPositionChange(function(newPos, oldPos)
+    if oldPos.z ~= posz() then return end
+    if oldPos then
+        local tile = g_map.getTile(oldPos)
+        if toggle.isOn() and tile:isWalkable() then
+            useWith(3180, tile:getTopUseThing())
+            toggle.setOff()
+        end
+    end
+end)
+
+local toggle2 = macro(10, "MW Target Step", "F11", function() end) -- taca mw no sqm onde o alvo esta
+
+onCreaturePositionChange(function(creature, newPos, oldPos)
+    if creature == target() or creature == g_game.getFollowingCreature() then
+        if oldPos and oldPos.z == posz() then
+            local tile2 = g_map.getTile(oldPos)
+            if toggle2.isOn() and tile2:isWalkable() then
+                useWith(3180, tile2:getTopUseThing())
+                toggle2.setOff()
+            end 
+        end
+    end
+end)
+
+
+
+UI.Separator()
+
+local dropItems = { 3492, 3031, 3035 }
+local maxStackedItems = 10
+local dropDelay = 600
+
+gpAntiPushDrop = macro(dropDelay , "Anti push", function ()
+  antiPush()
+end)
+
+onPlayerPositionChange(function()
+    antiPush()
+end)
+
+function antiPush()
+  if gpAntiPushDrop:isOff() then
+    return
+  end
+
+  local tile = g_map.getTile(pos())
+  if tile and tile:getThingCount() < maxStackedItems then
+    local thing = tile:getTopThing()
+    if thing and not thing:isNotMoveable() then
+      for i, item in pairs(dropItems) do
+        if item ~= thing:getId() then
+            local dropItem = findItem(item)
+            if dropItem then
+              g_game.move(dropItem, pos(), 1)
+            end
+        end
+      end
+    end
+  end
+end
+
+local function checkPos(x, y)
+ xyz = g_game.getLocalPlayer():getPosition()
+ xyz.x = xyz.x + x
+ xyz.y = xyz.y + y
+ tile = g_map.getTile(xyz)
+ if tile then
+  return g_game.use(tile:getTopUseThing())  
+ else
+  return false
+ end
+end
+
+
+UI.Separator()
+
+
+function staminaItems(parent)
+  if not parent then
+    parent = panel
+  end
+  local panelName = "staminaItemsUser"
+  local ui = setupUI([[
+Panel
+  height: 65
+  margin-top: 2
+
+  SmallBotSwitch
+    id: title
+    anchors.left: parent.left
+    anchors.right: parent.right
+    anchors.top: parent.top
+    text-align: center
+
+  HorizontalScrollBar
+    id: scroll1
+    anchors.left: parent.left
+    anchors.right: parent.horizontalCenter
+    anchors.top: title.bottom
+    margin-right: 2
+    margin-top: 2
+    minimum: 0
+    maximum: 42
+    step: 1
+    
+  HorizontalScrollBar
+    id: scroll2
+    anchors.left: parent.horizontalCenter
+    anchors.right: parent.right
+    anchors.top: prev.top
+    margin-left: 2
+    minimum: 0
+    maximum: 42
+    step: 1    
+
+  ItemsRow
+    id: items
+    anchors.left: parent.left
+    anchors.right: parent.right
+    anchors.top: prev.bottom
+  ]], parent)
+  ui:setId(panelName)
+
+  if not storage[panelName] then
+    storage[panelName] = {
+      min = 0,
+      max = 40,
+    }
+  end
+
+  local updateText = function()
+    ui.title:setText("" .. storage[panelName].min .. " <= Stamina >= " .. storage[panelName].max .. "")  
+  end
+ 
+  ui.scroll1.onValueChange = function(scroll, value)
+    storage[panelName].min = value
+    updateText()
+  end
+  ui.scroll2.onValueChange = function(scroll, value)
+    storage[panelName].max = value
+    updateText()
+  end
+ 
+  ui.scroll1:setValue(storage[panelName].min)
+  ui.scroll2:setValue(storage[panelName].max)
+ 
+  ui.title:setOn(storage[panelName].enabled)
+  ui.title.onClick = function(widget)
+    storage[panelName].enabled = not storage[panelName].enabled
+    widget:setOn(storage[panelName].enabled)
+  end
+ 
+  if type(storage[panelName].items) ~= 'table' then
+    storage[panelName].items = { 11588 }
+  end
+ 
+  for i=1,5 do
+    ui.items:getChildByIndex(i).onItemChange = function(widget)
+      storage[panelName].items[i] = widget:getItemId()
+    end
+    ui.items:getChildByIndex(i):setItemId(storage[panelName].items[i])    
+  end
+ 
+  macro(500, function()
+    if not storage[panelName].enabled or stamina() / 60 < storage[panelName].min or stamina() / 60 > storage[panelName].max then
+      return
+    end
+    local candidates = {}
+    for i, item in pairs(storage[panelName].items) do
+      if item >= 100 then
+        table.insert(candidates, item)
+      end
+    end
+    if #candidates == 0 then
+      return
+    end    
+    use(candidates[math.random(1, #candidates)])
+  end)
+end
+staminaItems()
+
+
+UI.Separator()
+ 
+ 
+local UE = "Magia de Area"
+
+if not storage[UE .. name] then
+storage[UE .. name] = {
+    AOEspell = "Exevo gran mas pox"
+}
+end
+
+macro(1000, "Safe SD/Mas Vis", function()
+  local target = g_game.getAttackingCreature()
+  if not target then return end
+  if isSafe(8) and manapercent() >= 70 then
+    say(storage[UE .. name].AOEspell)
+  else
+    usewith(3155, g_game.getAttackingCreature())
+  end
+end)
+
+addTextEdit("AOEspell", storage[UE .. name].AOEspell or "Exevo gran mas pox", function(widget, uetext)
+    storage[UE .. name].AOEspell = uetext
 end)
 
 
