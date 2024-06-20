9e177debb074b7f29e6cf6fa769d1350ec0aae1f
NotXina <157437731+NotXina@users.noreply.github.com>
2024-06-20 01:37:12 -0300

    Update XinaPackNew.lua

--- XinaPackNew.lua
+++ XinaPackNew.lua
@@ -34,20 +34,48 @@ addIcon("(RP) PvP", {item=25757, text="(RP) PvP"}, function(icon, isOn)
 Pally_PvP.setOn(isOn)
 end)
 
+local sellWand = 651
 
-local wand = 651 -- wand ID 
-local itemsIds = {3554, 3369, 3567, 3420, 3414, 3366, 3364, 3392, 3320, 3281, 7407, 7387, 3079, 826, 10387, 10385, 3006, 3360, 3324, 3381, 3382, 3419, 3385, 3386} -- ID items to sell 
-Seller = addIcon("Seller", {item = 651, text = "Seller"},
-  macro(100, function()
-  for i, itemId in pairs(itemsIds) do
-    local item = findItem(itemId)
-    if item then
-      useWith(wand, item)
-      return
+if type(storage.ItemsToSell) ~= "table" then
+  storage.ItemsToSell = {822, 7412, 7388, 3554, 7423, 7422, 32208, 32209, 8074, 821, 823, 32187, 32188, 16126, 3364, 3281, 3366, 7422, 7423, 3071, 3280, 3420, 3079, 3392, 7402, 3386, 32188, 32185, 32187, 32186, 3360, 3342, 3370, 7430, 3281, 8057, 3414, 32180, 32179, 32178, 3063, 826, 7382, 41971, 41968, 41969, 41970, 41966, 41965, 41967}
+end
+
+addSeparator()
+addLabel("","Items To Sell")
+addSeparator()
+local ItemsToSellContainer = UI.Container(function(widget, items)
+  storage.ItemsToSell = items
+end, true)
+addSeparator()
+ItemsToSellContainer:setHeight(90)
+ItemsToSellContainer:setItems(storage.ItemsToSell)
+
+local getContainerItemsIds = function(data)
+  local idsTable = {}
+  local data = data or {}
+  for _, item in ipairs(data) do
+    if type(item) ~= "number" then
+      table.insert(idsTable, item.id)
+    elseif type(item) == "number" then
+      table.insert(idsTable, item)
     end
   end
-end))
+  return idsTable
+end
 
+local sellMacro = macro(50, function()
+  local containers = getContainers()
+  for i, container in pairs(containers) do
+    for j, item in ipairs(container:getItems()) do
+      if table.find(getContainerItemsIds(storage.ItemsToSell), item:getId()) then
+        useWith(sellWand, item)
+        return delay(250)
+      end
+    end
+  end
+end)
+
+addIcon("SellIcon", {item={id=sellWand, count=1}, text= "Sell"}, sellMacro)
 
 local cIcon = addIcon("cI",{text="Cave\nBot",switchable=false,moveable=true}, function()
   if CaveBot.isOff() then 
@@ -870,54 +898,3 @@ Panel
 end
 comboScript()
 addSeparator()
-
-
-addSeparator()
-
-UI.Label("Mineracao")
-local mineableIds = {4461, 4460, 4465, 4464} 
-local pickId = 3456 
-local useDistance = 1 
-local moveDist = 5 
-
-local function getClosestPosition(positions)
-    local closestTile
-    local closestTileDistance = 99999
-    for _, position in ipairs(positions) do
-      local tileDist = getDistanceBetween(pos(), position)
-      if tileDist < closestTileDistance then
-        closestTile = position
-        closestTileDistance = tileDist
-      end
-    end
-    if closestTile then
-      return closestTile
-    end
-end
-
-macro(1, "Mining", function()
-    local possibleMine = {}
-    local foundMine = 0
-    for i, tile in ipairs(g_map.getTiles(posz())) do
-        if tile:getTopUseThing() ~= nil then
-            local topID = tile:getTopUseThing():getId()
-            minable = table.contains(mineableIds, topID)
-            if minable then
-                local distance = getDistanceBetween(pos(), tile:getPosition())
-                if (distance <= useDistance) then
-                    return usewith(pickId, tile:getTopUseThing())
-                elseif distance > useDistance and distance <= moveDist then
-                    if findPath(pos(), tile:getPosition(), moveDist, {ignoreNonPathable=true, precision=1}) then 
-                        table.insert(possibleMine, tile:getPosition())
-                        foundMine = foundMine + 1
-                    end
-                end
-            end
-        end
-    end
-  if foundMine >= 1 then
-        CaveBot.delay(20)
-        TargetBot.delay(20)
-        return autoWalk(getClosestPosition(possibleMine), moveDist, {ignoreNonPathable=true, precision=1})
-    end
-end)
